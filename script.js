b45565303a5c4771257cb1da43482e8175f0de81
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-02 11:48:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species =  species;
+	}
 
-class Dog extends Animal {}
+	getSpecies(){
+		return this.species;
+	}
+	
+		makeSound(){
+			 
+	}
+}
 
-class Cat extends Animal {}
+class Dog extends Animal {
+	bark(){
+		console.log("woof");
+	}
+}
+
+class Cat extends Animal {
+	 purr(){
+		console.log("purr");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
